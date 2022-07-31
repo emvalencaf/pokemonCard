7e31c9d84a534859f01263b7b1f50f3749250224
emvalencaf <edsonmvf@gmail.com>
2022-07-31 00:38:09 -0300

    tentativa de melhorar o css

--- js/View/Pokemon.view.js
+++ js/View/Pokemon.view.js
@@ -2,18 +2,21 @@ export class PokemonView{
     constructor(card){
         this.container = card
     }
+
     render(pokemon){
+
+        this.container.innerHTML = ""
         console.log(pokemon)
         const html = `
-        <div>
-            <h2 class="title"> ${pokemon.id}- ${pokemon.name}</h2> ${this.renderTypeList(pokemon.type)}
+        <div class="headerCard">
+            <h2 class="title"> ${pokemon.id}</span> ${pokemon.name}</h2> ${this.renderTypeList(pokemon.type)}
         </div>
-        <div class="pokemonContainer">
+        <div class="pokemonContainer pokemonType ${pokemon.type[0].type.name}">
             <img src="${pokemon.sprite}" alt="imagem do pokemon nº ${pokemon.id}: ${pokemon.name}">
         </div>
         <div class="pokedexEntry">
-            <h3>Pokedex entry:</h3>
-            <p class="texto">
+            <h3 class="title">Pokedex entry:</h3>
+            <p class="text">
                 ${pokemon.entry}
             </p>
         </div>
