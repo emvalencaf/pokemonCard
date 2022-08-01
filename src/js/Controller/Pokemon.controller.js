643d21fd0a846f81f7fa60c00e8cc6381d670711
emvalencaf <edsonmvf@gmail.com>
2022-08-01 20:34:30 -0300

    validação no search

--- src/js/Controller/Pokemon.controller.js
+++ src/js/Controller/Pokemon.controller.js
@@ -5,10 +5,23 @@ export class PokemonController{
     }
 
     async getPokemon(uri){
-        console.log("controller")
+
+        uri = this.uriValidation(uri)
+
+        console.log("controller", typeof uri)
         await this.service.getPokemon(uri)
         await this.service.getPokemonEntry(uri)
         this.view.render(this.service.pokemon)
+    }
+
+    uriValidation(uri){
+        const regex = /\d+/
+        const regex2 = /\w/
+
+        if(regex.test(uri)) return uri = Number(uri)
+
+        if(regex2.test(uri)) return uri = uri.toLowerCase()
+
     }
 
     renderBackgroundImage(){
